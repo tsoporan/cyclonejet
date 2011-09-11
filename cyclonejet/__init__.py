a80b95f64a17c4bc5c506313fe94e66b4ad2e836
Titus Soporan <titus@tsoporan.com>
2011-09-11 18:39:15 -0400

    Switch to Blueprints, custom 404 handler.

--- cyclonejet/__init__.py
+++ cyclonejet/__init__.py
@@ -1,12 +1,26 @@
 # -*- encoding:utf-8 -*-
-from flask import Flask
+from flask import Flask, render_template
+
 from cyclonejet.views.frontend import frontend
+from cyclonejet.views.errors import errors
+
 from flaskext.sqlalchemy import SQLAlchemy
 import settings
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = settings.DB_URI
-app.register_module(frontend)
 
+#Blueprint registration
+app.register_blueprint(frontend)
+app.register_blueprint(errors)
+
+#DB intialization
 db = SQLAlchemy(app)
+
+
+#Custom Error handling (move this somewhere else?)
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
