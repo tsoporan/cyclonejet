9f4f691d32b270de902e32c7550003bbef48ae5f
Titus Soporan <titus@tsoporan.com>
2011-09-13 21:52:33 -0400

    Added config file, seperated app factory to file

---
+++ cyclonejet/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from cyclonejet.views.frontend import frontend
+from cyclonejet.extensions import db
+
+from cyclonejet.config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    #Blueprint registration
+    app.register_blueprint(frontend)
+
+    #DB intialization
+    db.app = app
+    db.init_app(app)
+
+    #Custom Error handling (move this somewhere else?)
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('404.html'), 404
+
+    return app
+
