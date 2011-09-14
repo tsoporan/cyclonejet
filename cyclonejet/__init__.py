85da9822e3b5a51e707bc21e25bed4b1c0758d5a
Titus Soporan <titus@tsoporan.com>
2011-09-13 21:56:05 -0400

    Uneeded startup code from __init__

--- cyclonejet/__init__.py
+++ cyclonejet/__init__.py
@@ -1,24 +1,3 @@
 # -*- encoding:utf-8 -*-
-from flask import Flask, render_template
-
-from cyclonejet.views.frontend import frontend
-
-from flaskext.sqlalchemy import SQLAlchemy
-import settings
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = settings.DB_URI
-
-#Blueprint registration
-app.register_blueprint(frontend)
-
-#DB intialization
-db = SQLAlchemy(app)
-
-
-#Custom Error handling (move this somewhere else?)
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
+from cyclonejet.app import create_app
 
