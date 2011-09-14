1d300727179eff1c144e8132e1652a3809ef54f9
Titus Soporan <titus@tsoporan.com>
2011-09-13 21:55:20 -0400

    Config object

--- cyclonejet/config.py
+++ cyclonejet/config.py
@@ -1,15 +1,14 @@
 # -*- encoding:utf-8 -*-
-#Settings for Cyclone Jet
-import os
 
-PROJECT_PATH = ''
+class Config(object):
+   
+    DEBUG = True #for devel
 
-SECRET_KEY = 'U9F\'a[U\\:byA;N_^OGX5WG+|Nx|xf6"^'
+    SECRET_KEY = 'U9F\'a[U\\:byA;N_^OGX5WG+|Nx|xf6"^'
 
+    #SQLAlchemy Settings
+    DB_URI = 'sqlite:///dev.db'
 
-#SQLAlchemy Settings
-DB_URI = 'sqlite:///dev.db'
-
-#WTForms Settings
-CSRF_ENABLED = True
-CSRF_SESSION_KEY = '_csrf_token'
+    #WTForms Settings
+    CSRF_ENABLED = True
+    CSRF_SESSION_KEY = '_csrf_token'
