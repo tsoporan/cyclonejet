7e190b552f8c3f02ab0bea2b8598c8f553ee7ae4
Titus Soporan <titus@tsoporan.com>
2011-09-16 19:40:56 -0400

    registration form validation

--- cyclonejet/forms/registration.py
+++ cyclonejet/forms/registration.py
@@ -1,8 +1,19 @@
-from flaskext.wtf import Form, TextField, PasswordField, validators
+from flaskext.wtf import Form, TextField, PasswordField, validators, ValidationError
+from cyclonejet.models import User
 
 class RegistrationForm(Form):
 
     username = TextField(u"Username", validators=[validators.Required(), validators.Length(min=2)])
     email = TextField(u"Email", validators=[validators.Required(), validators.Email()])
     password = PasswordField(u"Password", validators=[validators.Required()])
-    
+
+    def validate_username(self, field):
+        user = User.query.filter_by(username=field.data).first()
+        if user:
+            raise ValidationError("This username already exists.")
+
+    def validate_email(self, field):
+        user = User.query.filter_by(email=field.data).first()
+        if user:
+            raise ValidationError("This email already exists for a user.")
+
