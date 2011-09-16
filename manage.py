2e004c8e0f6d3baa4943d2920850572eea918d29
Spencer Judd <spencercjudd@gmail.com>
2011-09-16 11:05:55 -0400

    abstracted json imports

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from flask import current_app
 from flaskext.script import Manager
 from cyclonejet import create_app
 from cyclonejet.extensions import db
-from cyclonejet.models import User, Anime
+from cyclonejet.models import User, Anime, Manga
 
 app = create_app('cyclonejet')
 manager = Manager(app)
@@ -18,17 +18,15 @@ def create_db():
 def drop_db():
     db.drop_all()
 
-@manager.command
-def populate_anime(fp):
-    """ Populate the Anime database from json. """
+def import_json(fp, model_class):
 
     import os
     assert os.path.exists(fp), "That file path doesn't seem to exist."
 
     try:
         import simplejson
-        animes = simplejson.load(open(fp))
-    except: #this needs to be json
+        entries = simplejson.load(open(fp))
+    except: # this needs to be json
         raise
 
     user = User.query_class.create_user(
@@ -37,27 +35,58 @@ def populate_anime(fp):
             password='changeme'
     )
 
-    for a in animes:
-        anime = Anime(
-            title = a['title'],
-            description = a['description'],
-        )
-        anime.user = user
-        db.session.add(anime)
-        
+    for e in entries:
+
+        # this could be cleaned up a bit once we have the mangareader
+        # spider returning cleaner data - @scjudd
+
+        if model_class.__name__ == 'Manga':
+            try:
+                description = e['description'][0],
+            except IndexError:
+                description = e['description'],
+
+            entry = model_class(
+                title = e['title'],
+                description = description,
+                year = e['year'],
+                author = e['author']
+            )
+        elif model_class.__name__ == 'Anime':
+            entry = model_class(
+                title = e['title'],
+                description = e['description']
+            )
+        else:
+            raise
+
+        entry.user = user
+        db.session.add(entry)
+
         try:
             db.session.commit()
-        except Exception, e: #these are most-likely dupes
+        except Exception, e: # these are most-likely dupes
             print(e.message)
             db.session.rollback()
 
-        print('Added anime: {}'.format(anime.title))
+        try:
+            print('Added {}: {}'.format(entry.__class__.__name__, entry.title))
+        except UnicodeEncodeError:
+            print('Added {}: {}'.format(entry.__class__.__name__, '[Error: UnicodeEncodeError]'))
 
     print('All done!')
 
+
+@manager.command
+def populate_anime(fp):
+    """ Populate the Anime database from json. """
+    import_json(fp, Anime)
+
+
 @manager.command
 def populate_manga(fp):
-    pass
+    """ Populate the Manga database from json. """
+    import_json(fp, Manga)
 
 
 #prefill our shell with some defaults
