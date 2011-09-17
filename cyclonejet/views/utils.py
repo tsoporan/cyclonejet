844e2b5e6c6ffba20d111844ec45ef339933ef73
Kalon Hinds <kh.cubed@gmail.com>
2011-09-17 13:42:24 -0400

    fixed redirect url to accounts.login

--- cyclonejet/views/utils.py
+++ cyclonejet/views/utils.py
@@ -5,7 +5,7 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'logged_in' not in session or session['logged_in'] is None:
-            return redirect(url_for('.login'))
+            return redirect(url_for('accounts.login'))
         return f(*args, **kwargs)
     return decorated_function
 
