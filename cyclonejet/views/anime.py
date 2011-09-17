b6fbb8e16c926bd4697e9eb5d6a11f75ef57ebbf
Titus Soporan <titus@tsoporan.com>
2011-09-16 22:11:25 -0400

    Utilize PaginatedListView for manga and anime

--- cyclonejet/views/anime.py
+++ cyclonejet/views/anime.py
@@ -1,32 +1,10 @@
 # -*- encoding:utf-8 -*-
-from flask import Blueprint, url_for, redirect, abort, flash, session, request, render_template
-
-from cyclonejet.forms import RegistrationForm, LoginForm
+from flask import Blueprint
 from cyclonejet.models.anime import Anime
-from cyclonejet.extensions import db
+from cyclonejet.views.generic import PaginatedListView
 
 anime = Blueprint('anime', __name__)
 
-@anime.route('/', methods=['GET'])
-def index():
-    """ Return an index of all collected animes. """
-
-    NUM_PER_PAGE = 25
-
-    page = int(request.args.get('p', 1))
-    num_pages = (Anime.query.count() / NUM_PER_PAGE)
-    
-    if Anime.query.count() % NUM_PER_PAGE > 0:
-        num_pages += 1
-
-    if page < 1 or page > num_pages:
-        return abort(404)
-
-    animes = Anime.query.order_by('title').limit(NUM_PER_PAGE).offset((page-1)*NUM_PER_PAGE)
-
-    return render_template('entry-index.html',
-        entries = animes,
-        current_page = page,
-        num_pages = num_pages
-    )
+index = PaginatedListView.as_view('anime_list', template_name='entry-index.html', model=Anime)
+anime.add_url_rule('/', view_func=index)
 
--- cyclonejet/views/manga.py
+++ cyclonejet/views/manga.py
@@ -1,31 +1,10 @@
 # -*- encoding:utf-8 -*-
-from flask import Blueprint, url_for, redirect, abort, flash, session, request, render_template
-
-from cyclonejet.forms import RegistrationForm, LoginForm
+from flask import Blueprint
 from cyclonejet.models.manga import Manga
-from cyclonejet.extensions import db
+from cyclonejet.views.generic import PaginatedListView
 
 manga = Blueprint('manga', __name__)
 
-@manga.route('/', methods=['GET'])
-def index():
-    """ Return an index of all collected mangas. """
+index = PaginatedListView.as_view('anime_list', template_name='entry-index.html', model=Manga)
+manga.add_url_rule('/', view_func=index)
 
-    NUM_PER_PAGE = 25
-
-    page = int(request.args.get('p', 1))
-    num_pages = (Manga.query.count() / NUM_PER_PAGE)
-    
-    if Manga.query.count() % NUM_PER_PAGE > 0:
-        num_pages += 1
-
-    if page < 1 or page > num_pages:
-        return abort(404)
-
-    mangas = Manga.query.order_by('title').limit(NUM_PER_PAGE).offset((page-1)*NUM_PER_PAGE)
-
-    return render_template('entry-index.html',
-        entries = mangas,
-        current_page = page,
-        num_pages = num_pages
-    )
