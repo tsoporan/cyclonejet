1abfdd74ea784ff0b58b9c0494fad76f19cbaae8
Titus Soporan <titus@tsoporan.com>
2011-09-17 16:45:35 -0400

    PaginatedListView 'type' addition

--- cyclonejet/views/generic.py
+++ cyclonejet/views/generic.py
@@ -26,20 +26,21 @@ class PaginatedListView(ListView):
 
     def __init__(self, model, template_name, paginate_by=25):
         self.model = model
+        self.ctype = self.model.__name__.lower()
         self.template_name = template_name
         self.paginate_by = paginate_by
 
     def paginate(self):
-        
+        ctxt = {'type': self.ctype}
+
         page = int(request.args.get('p', 1))
         
         obj_count = self.model.query.count()
         num_pages = obj_count / self.paginate_by
 
         if not obj_count: #we can't do anything without objects
-            return {
-                'error': "There are no '%s' in the database, make sure to add some." % self.model.__name__,
-            }
+            ctxt['error'] = "There are no '%s' in the database, make sure to add some." % self.ctype
+            return ctxt
 
         if obj_count % self.paginate_by > 0:
             num_pages += 1
@@ -49,11 +50,12 @@ class PaginatedListView(ListView):
        
         objects = self.model.query.order_by('title').limit(self.paginate_by).offset((page - 1) * self.paginate_by)
 
-        return {
+        ctxt.update({
             'entries' : objects,
             'current_page' : page,
             'num_pages' : num_pages,
-        }
+        })
+        return ctxt
         
     def get_context(self):
         return self.paginate()
