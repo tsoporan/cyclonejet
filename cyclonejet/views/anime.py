f777cb6a995c0d58a202b03a0c1600ee8db261f0
Titus Soporan <titus@tsoporan.com>
2011-09-20 20:38:12 -0400

    added detail views for manga/anime

--- cyclonejet/views/anime.py
+++ cyclonejet/views/anime.py
@@ -1,10 +1,16 @@
 # -*- encoding:utf-8 -*-
-from flask import Blueprint
+from flask import Blueprint, render_template
 from cyclonejet.models.anime import Anime
 from cyclonejet.views.generic import PaginatedListView
 
 anime = Blueprint('anime', __name__)
 
-index = PaginatedListView.as_view('anime_list', template_name='entry-index.html', model=Anime)
-anime.add_url_rule('/', 'index', view_func=index)
+index = PaginatedListView.as_view('anime_list', template_name='anime/list.html', model=Anime)
 
+def detail(anime_id):
+    anime = Anime.query.get_or_404(anime_id)
+    return render_template('anime/detail.html', anime=anime)
+
+
+anime.add_url_rule('/', 'index', view_func=index)
+anime.add_url_rule('/<int:anime_id>/', 'detail', view_func=detail)
--- cyclonejet/views/manga.py
+++ cyclonejet/views/manga.py
@@ -1,10 +1,16 @@
 # -*- encoding:utf-8 -*-
-from flask import Blueprint
+from flask import Blueprint, render_template
 from cyclonejet.models.manga import Manga
 from cyclonejet.views.generic import PaginatedListView
 
 manga = Blueprint('manga', __name__)
 
-index = PaginatedListView.as_view('anime_list', template_name='entry-index.html', model=Manga)
+index = PaginatedListView.as_view('anime_list', template_name='manga/list.html', model=Manga)
+
+def detail(manga_id):
+    manga = Manga.query.get_or_404(manga_id)
+    return render_template('manga/detail.html', manga=manga)
+
 manga.add_url_rule('/', 'index', view_func=index)
+manga.add_url_rule('/<int:manga_id>/', 'detail', view_func=detail)
 
